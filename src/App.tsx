2386ab1fc2282a1a01a011735746b03937702568
Venus Lumanglas <vlumanglas30@gmail.com>
2024-02-27 16:39:44 +0800

    Add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { useEffect } from "react";
 import { Toaster } from "react-hot-toast";
 import { AddTodo, TodoList } from "./components";
 
 const App = () => {
+  useEffect(() => {
+    const date = new Date();
+    const yearElement = document.getElementById("current-year");
+    yearElement && (yearElement.innerHTML = date.getFullYear().toString());
+  }, []);
+
   return (
     <div>
       <Toaster position="bottom-center" />
